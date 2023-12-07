a5903cb6d9497f521896ce7e6ee8726f6b77765f
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2023-12-07 22:28:18 +0530

    done

---
+++ supermarket-backend/src/test/java/com/example/supermarketbackend/SupermarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supermarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
