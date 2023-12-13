aad6dd79c63d3e6be7a94f69c4b39609c2e36922
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2023-12-13 19:12:00 +0530

    product upload image - backend

--- supermarket-backend/src/main/java/com/example/supermarketbackend/controller/ProductController.java
+++ supermarket-backend/src/main/java/com/example/supermarketbackend/controller/ProductController.java
@@ -2,13 +2,22 @@ package com.example.supermarketbackend.controller;
 
 import com.example.supermarketbackend.dto.ProductDto;
 import com.example.supermarketbackend.service.ProductService;
+import com.example.supermarketbackend.service.impl.FileUpload;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -18,7 +27,57 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private FileUpload fileUpload;
 
+
+    //Upload a image
+    @PostMapping("/images/{id}")
+    public ResponseEntity<?> uploadImageProduct(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+
+        ProductDto product = productService.getProductById(id);
+
+        String imageName = null;
+
+        try {
+            String uploadImage =  fileUpload.uploadImage("supermarket-ui/public/images/", file);
+            product.setImage(uploadImage);
+            ProductDto updateProduct =   productService.updateProduct(product, id, product.getCategory().getId());
+
+            return new  ResponseEntity<>(updateProduct, HttpStatus.ACCEPTED);
+
+        }catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(Map.of("Message","File not Upload in server"),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+
+    //download a image
+    @GetMapping("/images/download/{id}")
+    public ResponseEntity<InputStreamResource> downloadImageProduct(@PathVariable Long id) {
+        ProductDto product = productService.getProductById(id);
+
+        try {
+            String imagePath = "supermarket-ui/public/images/" + product.getImage(); // Assuming the image name is stored in the imageName field
+            File file = new File(imagePath);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG); // Set the appropriate content type based on your image type
+            headers.setContentDispositionFormData("attachment", file.getName());
+
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(null);
+        }
+    }
 
 
     @GetMapping
