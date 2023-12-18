53eedad72f791e6a4d83b8051adb6443b20294ad
Thushan W A R T <rusiththarindu2@gmail.com>
2023-12-18 15:16:03 +0530

    header corrected

--- supermarket-ui/src/components/Header.jsx
+++ supermarket-ui/src/components/Header.jsx
@@ -3,37 +3,28 @@ import "./styles/Header.css"
 
 const Header = () => {
   return (
-    <nav>
-            <div className="upper-nav">
-                <div className="nav-logo-div">
-                <a href="#"><img src="../public/logo3.png" alt="logo" className="nav-logo"/></a>
-                </div>
-                {/* <div className="search-bar-nav">
-                    <input type="text" placeholder="Search Products" className="search-bar"/>
-                </div> */}
+    <header>
+            
+            <div className="nav-content">
 
-            <div className="nav-links">
-              <ul>
-                  <li>
-                      <a href="#">Home</a>
-                      <a href="#">Categories</a>
-                      <a href="#">Packages</a>
-                      <a href="#" className="">View Cart</a>
-                      <a href="#">Login</a>
-                      
-                  </li>
-              </ul>
-            </div>
-                
-            </div>
-            {/* <div className="lower-nav">
-                <div className="dropdown">
-                    
+                <div className="nav-logo">
+                    <img src="public\logo3.png" alt="logo" />
                 </div>
-                
-            </div> */}
-        </nav>
-  )
+
+                <div className="nav-links">
+                    <ul>
+                        <li className="links"><a href="#">Home</a></li>
+                        <li className="links"><a href="#">Categories</a></li>
+                        <li className="links"><a href="#">Packages</a></li>
+                        <li className="links"><a href="#">View Cart</a></li>
+                        <li className="links"><a href="#">Login</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        
+        </header>
+  );
 }
 
 export default Header
