5e235cf7069ec6ae108c326b191985f572b0a891
NemalFdo <dinethnemal2002@gmail.com>
2023-12-19 22:06:18 +0530

    Manage Product page

--- supermarket-ui/src/Pages/Manage.jsx
+++ supermarket-ui/src/Pages/Manage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles/Manage.css'
+import './Page Styles/Manage.css'
 
 export const Manage = () => {
   return (
@@ -10,8 +10,69 @@ export const Manage = () => {
                 <h1>Manage Products</h1>
                 <div className="buttons">
                     <button onClick={()=>{window.location="/addProduct"}} className='btn1' >Add a new product</button>
-                    <button onClick={()=>{window.location="/manaCat"}} className='btn1' >Manage Category</button>
+                    <div className="btn-right">
+                    <button onClick={()=>{window.location="/manaCat"}} className='btn1' >View Orders</button>
+                    <button onClick={()=>{window.location="/manaCat"}} className='btn2' >Manage Category</button>
+                    </div>
                 </div>
+                    <div class="table-manage">
+                        <ul class="responsive-table">
+                          <li class="table-header">
+                            <div class="col col-1">Id</div>
+                            <div class="col col-2">Name</div>
+                            <div class="col col-3">Category</div>
+                            <div class="col col-4">Price(LKR)</div>
+                            <div class="col col-5">Image</div>
+                            <div class="col col-6">Edit</div>
+                            <div class="col col-7">Remove</div>
+                          </li>
+                          <li class="table-row">
+                            <div class="col col-1" data-label="Job Id">1</div>
+                            <div class="col col-2" data-label="Customer Name">Tomato</div>
+                            <div class="col col-3" data-label="Amount">Vegetables</div>
+                            <div class="col col-4" data-label="Payment Status">150</div>
+                            <div class="col col-5"><img src="https://images.pexels.com/photos/985"></img></div>
+                            <div class="col col-6"><button class="btn3">Edit</button></div>
+                            <div class="col col-6"><button class="btn3">Remove</button></div>
+                          </li>
+                          <li class="table-row">
+                            <div class="col col-1" data-label="Job Id">1</div>
+                            <div class="col col-2" data-label="Customer Name">Tomato</div>
+                            <div class="col col-3" data-label="Amount">Vegetables</div>
+                            <div class="col col-4" data-label="Payment Status">150</div>
+                            <div class="col col-5"><img src="https://images.pexels.com/photos/985"></img></div>
+                            <div class="col col-6"><button class="btn3">Edit</button></div>
+                            <div class="col col-6"><button class="btn3">Remove</button></div>
+                          </li>
+                          <li class="table-row">
+                            <div class="col col-1" data-label="Job Id">1</div>
+                            <div class="col col-2" data-label="Customer Name">Tomato</div>
+                            <div class="col col-3" data-label="Amount">Vegetables</div>
+                            <div class="col col-4" data-label="Payment Status">150</div>
+                            <div class="col col-5"><img src="https://images.pexels.com/photos/985"></img></div>
+                            <div class="col col-6"><button class="btn3">Edit</button></div>
+                            <div class="col col-6"><button class="btn3">Remove</button></div>
+                          </li>
+                          <li class="table-row">
+                            <div class="col col-1" data-label="Job Id">1</div>
+                            <div class="col col-2" data-label="Customer Name">Tomato</div>
+                            <div class="col col-3" data-label="Amount">Vegetables</div>
+                            <div class="col col-4" data-label="Payment Status">150</div>
+                            <div class="col col-5"><img src="https://images.pexels.com/photos/985"></img></div>
+                            <div class="col col-6"><button class="btn3">Edit</button></div>
+                            <div class="col col-6"><button class="btn3">Remove</button></div>
+                          </li>
+                          <li class="table-row">
+                            <div class="col col-1" data-label="Job Id">1</div>
+                            <div class="col col-2" data-label="Customer Name">Tomato</div>
+                            <div class="col col-3" data-label="Amount">Vegetables</div>
+                            <div class="col col-4" data-label="Payment Status">150</div>
+                            <div class="col col-5"><img src="https://images.pexels.com/photos/985"></img></div>
+                            <div class="col col-6"><button class="btn3">Edit</button></div>
+                            <div class="col col-6"><button class="btn3">Remove</button></div>
+                          </li>
+                        </ul>
+                      </div>
             </div>
         </div>
     </div>
