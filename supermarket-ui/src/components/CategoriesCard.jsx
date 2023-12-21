fb7728c2d0ccd8e2df40c52c0074178eb5c26587
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2023-12-21 16:48:00 +0530

    order frontend

--- supermarket-ui/src/components/CategoriesCard.jsx
+++ supermarket-ui/src/components/CategoriesCard.jsx
@@ -1,32 +1,39 @@
 import "./styles/CategoriesCard.css"
+import  {useNavigate} from "react-router-dom";
 
 const CategoriesCard = () =>{
+
+    const navigate = useNavigate();
+
     return(
         <div className="category-section">
             <br /><br />
             <h1 className="main-header">Popular Categories</h1>
-            <br />
+        <div className="category-more-hero">
+            <h3 className="category-more"  onClick={() => {navigate('/shop')}}>See more {'>>'}</h3>
+        </div>
             <div className="block-container">
-                <div className="left-most-div common">
+                
+                <div className="left-most-div common" onClick={() => {navigate('/shop')}}>
                     <img src="public\snacks.png" alt="snacks image" className="snacks-img"/>
                     <h3 className="snacks-text">Snacks</h3>
                 </div>
                 <div className="left-block">
-                    <div className="top-cate common">
+                    <div className="top-cate common"  onClick={() => {navigate('/shop')}}>
                         <img src="public\beauty.png" alt="Beauty Product Image" className="beauty-img"/>
                         <h3 className="beauty-text">Beauty Products</h3>
                     </div>
-                    <div className="bottom-cate common">
+                    <div className="bottom-cate common"  onClick={() => {navigate('/shop')}}>
                         <img src="public\fish.png" alt="Fish Image" className="fish-img"/>
                         <h3 className="fish-text">Fish</h3>
                     </div>
                 </div>
                 <div className="right-block">
-                    <div className="left-cate common">
+                    <div className="left-cate common"  onClick={() => {navigate('/shop')}}>
                         <img src="public\caba.png" alt="Cabbage" className="vege-img"/>
                         <h3 className="vege-text">Vegetables</h3>
                     </div>
-                    <div className="right-cate common">
+                    <div className="right-cate common"  onClick={() => {navigate('/shop')}}>
                         <img src="public\meat.png" alt="Raw meat" className="meat-img"/>
                         <h3 className="meat-text">Meat</h3>
                     </div>
